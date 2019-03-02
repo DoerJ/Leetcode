c5f56ab28859ad515c156144a9cb2b22515bbbf6
jamesHe <jamesyuhao@hotmail.com>
2019-03-02 00:28:36 -0800

    zig-zag updated

--- sol-medium.js
+++ sol-medium.js
@@ -1,6 +1,6 @@
 /**
 Zig-Zag conversion
-The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
 (you may want to display this pattern in a fixed font for better legibility)
 P   A   H   N
 A P L S I I G
@@ -14,57 +14,57 @@ And then read line by line: "PAHNAPLSIIGYIR"
  * @return {string}
  */
 var convert = function(s, numRows) {
-    // a 2-dimensional array
-    var conversion = "";
-    var matrix = new Array();
-    var marginCols = (numRows - 2 < 0) ? 0 : numRows - 2;
-    var colFlag = true;
-    var strLength = s.length;
-    var i = 0;
-    var posIndicator;
-    while(i < strLength) {
-        var colArray = new Array();
-        if(colFlag) {
-            var j = 0;
-          	posIndicator = marginCols;
-            while(j < numRows) {
-                if(i < strLength) {
-                    colArray.push(s.charAt(i));
-                  	i++;
-                }
-                else {
-                    colArray.push(" ");
-                }
-                j++;
-            }
-            colFlag = (marginCols === 0) ? true : false;
-        }
-        else {
-            var k = 0;
-            while(k < numRows) {
-                if(k === posIndicator) {
-                    colArray.push(s.charAt(i));
-                }
-                else {
-                    colArray.push(" ");
-                }
-                k++;
-            }
-          	posIndicator--;
-          	colFlag = (posIndicator === 0) ? true : false;
-            i++;
-        }
-        matrix.push(colArray);
-    }
-    // parse matrix
-    var matrixLength = matrix.length;
-    var p;
-    var q;
-    for(p = 0; p < numRows; p++) {
-        for(q = 0; q < matrixLength; q++) {
-            conversion += matrix[q][p];
-        }
-    }
-    conversion = conversion.split(' ').join('');
-    return conversion;
+  // a 2-dimensional array
+  var conversion = "";
+  var matrix = new Array();
+  var marginCols = (numRows - 2 < 0) ? 0 : numRows - 2;
+  var colFlag = true;
+  var strLength = s.length;
+  var i = 0;
+  var posIndicator;
+  while(i < strLength) {
+      var colArray = new Array();
+      if(colFlag) {
+          var j = 0;
+        	posIndicator = marginCols;
+          while(j < numRows) {
+              if(i < strLength) {
+                  colArray.push(s.charAt(i));
+                	i++;
+              }
+              else {
+                  colArray.push(" ");
+              }
+              j++;
+          }
+          colFlag = (marginCols === 0) ? true : false;
+      }
+      else {
+          var k = 0;
+          while(k < numRows) {
+              if(k === posIndicator) {
+                  colArray.push(s.charAt(i));
+              }
+              else {
+                  colArray.push(" ");
+              }
+              k++;
+          }
+        	posIndicator--;
+        	colFlag = (posIndicator === 0) ? true : false;
+          i++;
+      }
+      matrix.push(colArray);
+  }
+  // parse matrix
+  var matrixLength = matrix.length;
+  var p;
+  var q;
+  for(p = 0; p < numRows; p++) {
+      for(q = 0; q < matrixLength; q++) {
+          conversion += matrix[q][p];
+      }
+  }
+  conversion = conversion.split(' ').join('');
+  return conversion;
 };
