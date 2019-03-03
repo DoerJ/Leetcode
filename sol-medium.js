03e297f95d807adb7f6043aebf415f1bad1542dd
jamesHe <jamesyuhao@hotmail.com>
2019-03-02 18:59:03 -0800

    container-with-water added

--- sol-medium.js
+++ sol-medium.js
@@ -1,5 +1,5 @@
 /**
-Zig-Zag conversion
+Zig-Zag Conversion
 The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
 (you may want to display this pattern in a fixed font for better legibility)
 P   A   H   N
@@ -97,4 +97,44 @@ var lengthOfLongestSubstring = function(s) {
     }
   }
   return longestSub;
+};
+
+
+/**
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+@param {number[]} height
+@return {number}
+*/
+var maxArea = function(height) {
+
+};
+
+
+/**
+Container With Most Water
+Given n non-negative integers a1, a2, ..., an ,
+where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+@param {number[]} height
+@return {number}
+*/
+var maxArea = function(height) {
+  var left_pointer = 0;
+  var right_pointer = height.length - 1;
+  var i = height.length - 2;
+  var maxArea = 0;
+  while(i >= 0) {
+    maxArea = Math.max(maxArea, (right_pointer - left_pointer) *
+    Math.min(height[left_pointer], height[right_pointer]));
+    if(height[left_pointer] <= height[right_pointer]) {
+      left_pointer++;
+    }
+    else {
+      right_pointer--;
+    }
+    i--;
+  }
+  return maxArea;
 };
