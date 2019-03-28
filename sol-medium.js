d0a947d35ccb00dca3a19d77ef91b5d726f4dc56
jamesHe <jamesyuhao@hotmail.com>
2019-03-27 18:32:12 -0700

    permute added

--- sol-medium.js
+++ sol-medium.js
@@ -383,3 +383,51 @@ var searchRange = function(nums, target) {
     }
     return range;
 };
+
+/**
+Given a collection of distinct integers, return all possible permutations.
+Example:
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    var sol = new Array();
+    if(nums.length === 0) {
+        return sol;
+    }
+    var i;
+    for(i = 0; i < nums.length; i++) {
+        var sub_sol = new Array();
+        var temp_nums = nums;
+        temp_nums.splice(i, 1);
+        sub_sol.push(nums[i]);
+        backtrackingSearch(sub_sol, temp_nums, sol);
+    }
+    return sol;
+};
+
+var backtrackingSearch = function(sub_sol, remainings, sol) {
+    // if lowest level of tree
+    if(remainings.length === 0) {
+        sol.push(sub_sol);
+    }
+    else {
+        var j;
+        for(j = 0; j < remainings.length; j++) {
+            sub_sol.push(remainings[j]);
+            var temp_remainings = remainings;
+            temp_remainings.splice(j, 1);
+            backtrackingSearch(sub_sol, temp_remainings, sol);
+        }
+    }
+}
