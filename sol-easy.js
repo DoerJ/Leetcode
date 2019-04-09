b39347d588bda55da23e6c830688aeefb282efcb
james <James.He@arlo.com>
2019-04-08 17:31:46 -0700

    move_zero added

--- sol-easy.js
+++ sol-easy.js
@@ -463,3 +463,31 @@ var isPalindrome = function(head) {
     }
     return true;
 };
+
+/**
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    // indicate the first pos of zero
+    var pos = 0;
+    var i;
+    for(i = 0; i < nums.length; i++) {
+        if(nums[i] !== 0) {
+            nums[pos] = nums[i];
+            pos++;
+        }
+    }
+    var j;
+    console.log(pos)
+    for(j = pos; j < nums.length; j++) {
+        nums[j] = 0;
+    }
+    return nums;
+};
