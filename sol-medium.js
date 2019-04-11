a69b5d5c267b2977401fc459486a1ec25788a988
jamesHe <jamesyuhao@hotmail.com>
2019-04-10 21:57:32 -0700

    find_duplicate  added

--- sol-medium.js
+++ sol-medium.js
@@ -935,3 +935,29 @@ var topKFrequent = function(nums, k) {
     }
     return sols;
 };
+
+/**
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive),
+prove that at least one duplicate number must exist.
+Assume that there is only one duplicate number, find the duplicate one.
+
+Example 1:
+
+Input: [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: [3,1,3,4,2]
+Output: 3
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+
+    var map = new Map();
+    var i;
+    for(i = 0; i < nums.length; i++) {
+        if(!map.has(nums[i])) map.set(nums[i], 1);
+        else return nums[i];
+    }
+};
