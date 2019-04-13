0555af05eebd0f88b340f414e056c943b191bc84
jamesHe <jamesyuhao@hotmail.com>
2019-04-13 15:49:08 -0700

    j_s added

--- sol-easy.js
+++ sol-easy.js
@@ -621,3 +621,35 @@ var singleNumber = function(nums) {
     }
     return nums[nums.length - 1];
 };
+
+/**
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.
+Each character in S is a type of stone you have.
+You want to know how many of the stones you have are also jewels.
+The letters in J are guaranteed distinct, and all characters in J and S are letters.
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+    // use has set
+    var set = new Set();
+    var count = 0;
+    var i;
+    for(i = 0; i < J.length; i++) {
+        set.add(J.charAt(i));
+    }
+
+    var j;
+    for(j = 0; j < S.length; j++) {
+        if(set.has(S.charAt(j))) count += 1;
+    }
+
+    return count;
+};
