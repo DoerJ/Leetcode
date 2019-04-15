51ca0719702702a2efdbfaf92f5be4900b6b5889
jamesHe <jamesyuhao@hotmail.com>
2019-04-14 19:03:45 -0700

    stock added

--- sol-easy.js
+++ sol-easy.js
@@ -748,3 +748,39 @@ var maxProfit = function(prices) {
     }
     return max;
 };
+
+/**
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+Note:
+
+Your returned answers (both index1 and index2) are not zero-based.
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+Example:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    // use two pointers
+    var left = 0;
+    var right = numbers.length - 1;
+    var sols = [];
+
+    while(left !== right) {
+        var sum = numbers[left] + numbers[right];
+        if(sum > target) right--;
+        else if(sum < target) left++;
+        else {
+            sols = [left + 1, right + 1];
+            break;
+        }
+    }
+
+    return sols;
+};
