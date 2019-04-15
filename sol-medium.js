a0675366f4c9ade86c2cb3c33d91a2cf0439c11d
jamesHe <jamesyuhao@hotmail.com>
2019-04-14 22:40:28 -0700

    link_cycle added

--- sol-medium.js
+++ sol-medium.js
@@ -1082,3 +1082,39 @@ var subarraySum = function(nums, k) {
     }
     return count;
 };
+
+/**
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    // use hash map
+    // corner cases
+    if(head === null || head.next === null) return null;
+
+    var map = new Map();
+    var index = 0;
+
+    var dummy = head;
+    while(dummy !== null) {
+        if(!map.has(dummy)) {
+            map.set(dummy, index);
+            dummy = dummy.next;
+            index += 1;
+        }
+        else return dummy;
+    }
+    return null;
+};
