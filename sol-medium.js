4cda045354b06982cefcec369a72e67789fbd756
james <James.He@arlo.com>
2019-04-18 15:47:46 -0700

    warmer temperature added

--- sol-medium.js
+++ sol-medium.js
@@ -1189,3 +1189,32 @@ var maxProduct = function(nums) {
 
     return Math.max(nums[0], sol);
 };
+
+/**
+Given a list of daily temperatures T, return a list such that,
+for each day in the input, tells you how many days you would have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+
+Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+ * @param {number[]} T
+ * @return {number[]}
+ */
+var dailyTemperatures = function(T) {
+    // use stack
+    
+    // initialization
+    var sols = new Array(T.length).fill(0);
+    var stack = [];
+
+    var i;
+    // note: the last t must be lower than the rest of t
+    for(i = 0; i < T.length; i++) {
+        var temperature = T[i];
+        while(stack.length !== 0 && temperature > T[stack[stack.length - 1]]) {
+            var index = stack.pop();
+            sols[index] = i - index;
+        }
+        stack.push(i);
+    }
+    return sols;
+};
